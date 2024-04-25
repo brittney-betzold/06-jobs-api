85786d212f2b5f85772910300b3b5f9e9655d52c
brittney betzold <brittney.betzold2022@gmail.com>
2024-04-25 15:22:55 -0500

    week9

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
+//connect to database
+const connectDB = require("./db/connect");
+const authenticateUser = require("./middleware/authentication");
+
+//routers
+const authRouter = require("./routes/auth");
+const recipieRouter = require("./routes/recipies");
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/recipies", authenticateUser, recipieRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
